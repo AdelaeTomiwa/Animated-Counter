9fd28a4f013a7d2b8e365b0b9023def952083eeb
Tomiwa Adelae <tommedia6001@gmail.com>
2020-10-10 22:06:48 -0200

    Final Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const counters = document.querySelectorAll('.counter');
+const speed = 200;
+
+counters.forEach((counter)=>{
+   const updateCount = ()=>{
+      const target = +counter.getAttribute('data-target');
+
+      const count = +counter.innerHTML;
+
+      const inc = target / speed;
+
+      if(count < target){
+         counter.innerHTML = Math.ceil(count + inc);
+
+         setTimeout(updateCount, 1)
+      }else{
+         count.innerHTML = target;
+      }
+   }
+
+   updateCount();
+})
